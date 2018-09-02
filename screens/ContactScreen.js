4b856244fd4d6bdd6326683d220be9b68596ec33
unknown <Rezerbiw12>
2018-09-02 19:52:17 +0700

    9/2/2018

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Container, Header, Left, Right, Icon, Title } from 'native-base'
+import { View, Text, StyleSheet } from 'react-native';
+import Button from 'react-native-button';
+import Modal from 'react-native-modalbox';
+import { Container, Header, Left, Right, Icon, Title, Footer, FooterTab, Button as NewButton, Badge, Text as NewText } from 'native-base'
+import { Icon as ElementIcon } from 'react-native-elements'
 
 class ContactScreen extends Component {
     static navigationOptions = {
@@ -20,11 +23,94 @@ class ContactScreen extends Component {
                     </Right>
                 </Header>
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text> ContactMe </Text>
+                    <Button onPress={() => this.refs.modal6.open()} style={styles.btn}>ติดต่อเรา</Button>
+                    <Modal style={[styles.modal4]} position={"bottom"} ref={"modal6"} swipeArea={20}>
+                        <Container>
+                            <Header>
+                                <Right>
+                                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                                        <Title>ติดต่อเรา</Title>
+                                    </View>
+                                </Right>
+                            </Header>
+                                <View style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+                                    <View style={{flexDirection: 'column',justifyContent:'center',alignItems:'center' }}>
+                                        <ElementIcon
+                                            reverse
+                                            name='ios-mail'
+                                            type='ionicon'
+                                            color='#517fa4'
+                                        />
+                                        <Text>
+                                            Email
+                                        </Text>
+                                    </View>
+                                    <View style={{flexDirection: 'column',justifyContent:'center',alignItems:'center' }}>
+                                        <ElementIcon
+                                            reverse
+                                            name='ios-chatbubbles'
+                                            type='ionicon'
+                                            color='#517fa4'
+                                        />
+                                        <Text>
+                                            Chat
+                                        </Text>
+                                    </View>
+                            </View>
+                        </Container>
+                    </Modal>
                 </View>
             </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+
+    wrapper: {
+        paddingTop: 50,
+        flex: 1
+    },
+
+    modal: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    modal2: {
+        height: 230,
+        backgroundColor: "#3B5998"
+    },
+
+    modal3: {
+        height: 300,
+        width: 300
+    },
+
+    modal4: {
+        height: 215
+    },
+
+    btn: {
+        margin: 10,
+        backgroundColor: "#3B5998",
+        color: "white",
+        padding: 10
+    },
+
+    btnModal: {
+        position: "absolute",
+        top: 0,
+        right: 0,
+        width: 50,
+        height: 50,
+        backgroundColor: "transparent"
+    },
+
+    text: {
+        color: "black",
+        fontSize: 22
+    }
+
+});
 export default ContactScreen;
